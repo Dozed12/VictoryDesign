dfeb4fa3afe4f8e357be42358d13ef1a5a4a3464
Dozed12 <up201607929@fe.up.pt>
2019-11-14 18:08:38 +0000

    Cleanup Autarky scripts

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -20,43 +20,7 @@ public class Game : MonoBehaviour
 
     public void SetupNewGame()
     {
-        //Clear resources
-        Nation.resources = new Dictionary<string, Resource>();
-
-        //Setup resources
-        Nation.resources.Add("Coal", new Resource("Coal",  Resource.Type.SOLID_FUEL));
-        Nation.resources.Add("Charcoal", new Resource("Charcoal",  Resource.Type.SOLID_FUEL));
-        Nation.resources.Add("Peat", new Resource("Peat",  Resource.Type.SOLID_FUEL));
-
-        Nation.resources.Add("Oil", new Resource("Oil",  Resource.Type.LIQUID_FUEL));
-        Nation.resources.Add("Bio-Oil", new Resource("Bio-Oil",  Resource.Type.LIQUID_FUEL));
-
-        Nation.resources.Add("Natural Gas", new Resource("Natural Gas",  Resource.Type.GAS_FUEL));
-        Nation.resources.Add("Wood Gas", new Resource("Wood Gas",  Resource.Type.GAS_FUEL));
-
-        Nation.resources.Add("Iron", new Resource("Iron",  Resource.Type.IRON));
-
-        Nation.resources.Add("Hardening Metals", new Resource("Hardening Metals",  Resource.Type.HARDENING_METALS));
-
-        Nation.resources.Add("Machining Metals", new Resource("Machining Metals",  Resource.Type.MACHINING_METALS));
-
-        Nation.resources.Add("Conductive Metals", new Resource("Conductive Metals",  Resource.Type.CONDUCTIVE_METALS));
-
-        Nation.resources.Add("Light Metals", new Resource("Light Metals",  Resource.Type.LIGHT_METALS));
-
-        Nation.resources.Add("Battery Metals", new Resource("Battery Metals",  Resource.Type.BATTERY_METALS));
-
-        Nation.resources.Add("Electricity", new Resource("Electricity",  Resource.Type.ELECTRICITY));
-
-        Nation.resources.Add("Catalysts", new Resource("Catalysts",  Resource.Type.CATALYSTS));
         
-        Nation.resources.Add("Sulphur", new Resource("Sulphur",  Resource.Type.NITRATES));
-        Nation.resources.Add("Potash", new Resource("Potash",  Resource.Type.NITRATES));
-
-        Nation.resources.Add("Rubber", new Resource("Rubber",  Resource.Type.RUBBER));
-
-        Nation.resources.Add("White Oil", new Resource("White Oil",  Resource.Type.HYDRAULIC_FLUID));
-        Nation.resources.Add("Hydraulic Bio Oil", new Resource("Hydraulic Bio Oil",  Resource.Type.HYDRAULIC_FLUID));
     }
 
 }
--- Assets/Scripts/Nation.cs
+++ Assets/Scripts/Nation.cs
@@ -4,19 +4,6 @@ using UnityEngine;
 
 public static class Nation
 {
-    //Industrial capacity available to the player
-    public static int industrialCapacity;
-
-    //Progress until surrender
-    public static float surrenderProgress;
-
-    //Lost territory in kilometers
-    public static int lostTerritory;
     
-    //Resources stored by name
-    public static Dictionary<string, Resource> resources;
-
-    //Extraction sites List
-    public static List<ExtractionSite> sites;
 
 }
