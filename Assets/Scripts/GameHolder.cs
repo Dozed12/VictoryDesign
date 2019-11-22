f6f793baeb7979363fab46832ccb4509702071b5
Dozed12 <up201607929@fe.up.pt>
2019-11-22 17:11:11 +0000

    Generate Base Designs; Identify Selected Design

--- Assets/Scripts/GameHolder.cs
+++ Assets/Scripts/GameHolder.cs
@@ -20,7 +20,7 @@ public class GameHolder : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Test Generate new Helmets on same Design Institute
+        //Test Generate new Helmets Design
         if (Input.GetKeyDown(KeyCode.R))
         {
             Helmet[] helmets = Game.us.RequestDesign(typeof(Helmet)).Cast<Helmet>().ToArray();
@@ -44,11 +44,25 @@ public class GameHolder : MonoBehaviour
         Game.us.AddInstitutes(new Type[] { typeof(SmallArm) }, 3);
         Game.us.AddInstitutes(new Type[] { typeof(MachineGun) }, 3);
 
+        //Add our base designs
+        Game.us.rifle = (Rifle)Game.us.RequestDesign(typeof(Rifle))[0];
+        Game.us.smallArm = (SmallArm)Game.us.RequestDesign(typeof(SmallArm))[0];
+        Game.us.uniform = (Uniform)Game.us.RequestDesign(typeof(Uniform))[0];
+        Game.us.helmet = (Helmet)Game.us.RequestDesign(typeof(Helmet))[0];
+        Game.us.machineGun = (MachineGun)Game.us.RequestDesign(typeof(MachineGun))[0];
+
         //Create Their Base Institutes
         Game.them.AddInstitutes(new Type[] { typeof(Helmet), typeof(Uniform) }, 3);
         Game.them.AddInstitutes(new Type[] { typeof(Rifle) }, 3);
         Game.them.AddInstitutes(new Type[] { typeof(SmallArm) }, 3);
         Game.them.AddInstitutes(new Type[] { typeof(MachineGun) }, 3);
+
+        //Add their base designs
+        Game.them.rifle = (Rifle)Game.them.RequestDesign(typeof(Rifle))[0];
+        Game.them.smallArm = (SmallArm)Game.them.RequestDesign(typeof(SmallArm))[0];
+        Game.them.uniform = (Uniform)Game.them.RequestDesign(typeof(Uniform))[0];
+        Game.them.helmet = (Helmet)Game.them.RequestDesign(typeof(Helmet))[0];
+        Game.them.machineGun = (MachineGun)Game.them.RequestDesign(typeof(MachineGun))[0];
     }
 
     // Select Design to show
@@ -60,7 +74,47 @@ public class GameHolder : MonoBehaviour
         string who = parts[0];
         string what = parts[1];
 
-        Debug.Log("Design Selected: " + who + " " + what);
+        //Identify side
+        Nation side;
+        switch (who)
+        {
+            case "our":
+                side = Game.us;
+                break;
+            case "their":
+                side = Game.them;
+                break;
+            default:
+                side = new Nation();
+                break;
+        }
+
+        //Identify design
+        Design selectedDesign;
+        switch (what)
+        {
+            case "rifle":
+                selectedDesign = side.rifle;
+                break;
+            case "smallArm":
+                selectedDesign = side.smallArm;
+                break;
+            case "uniform":
+                selectedDesign = side.uniform;
+                break;
+            case "helmet":
+                selectedDesign = side.helmet;
+                break;
+            case "machineGun":
+                selectedDesign = side.machineGun;
+                break;
+            default:
+                selectedDesign = new Helmet();
+                break;
+        }
+
+        //Print Selected Design
+        Utils.Dump(selectedDesign);
     }
 
 }
