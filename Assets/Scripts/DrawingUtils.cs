f59ba1bc1313e92b9f0c74f9d4928dc17b9c3814
Dozed12 <up201607929@fe.up.pt>
2019-12-02 19:42:50 +0000

    DrawingUtils with FloodFill

---
+++ Assets/Scripts/DrawingUtils.cs
@@ -0,0 +1,82 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public static class DrawingUtils
+{
+
+    public struct Point
+    {
+
+        public int x;
+        public int y;
+
+        public Point(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    public static void FloodFill(Texture2D input, Texture2D output, Color sourceColor, float tollerance, int x, int y)
+    {
+        var targetColor = Color.red;
+        var q = new Queue<Point>(input.width * input.height);
+        q.Enqueue(new Point(x, y));
+        int iterations = 0;
+
+        var width = input.width;
+        var height = input.height;
+        while (q.Count > 0)
+        {
+            var point = q.Dequeue();
+            var x1 = point.x;
+            var y1 = point.y;
+            if (q.Count > width * height)
+            {
+                throw new System.Exception("The algorithm is probably looping. Queue size: " + q.Count);
+            }
+
+            if (output.GetPixel(x1, y1) == targetColor)
+            {
+                continue;
+            }
+
+            output.SetPixel(x1, y1, targetColor);
+
+            var newPoint = new Point(x1 + 1, y1);
+            if (CheckValidity(input, input.width, input.height, newPoint, sourceColor, tollerance))
+                q.Enqueue(newPoint);
+
+            newPoint = new Point(x1 - 1, y1);
+            if (CheckValidity(input, input.width, input.height, newPoint, sourceColor, tollerance))
+                q.Enqueue(newPoint);
+
+            newPoint = new Point(x1, y1 + 1);
+            if (CheckValidity(input, input.width, input.height, newPoint, sourceColor, tollerance))
+                q.Enqueue(newPoint);
+
+            newPoint = new Point(x1, y1 - 1);
+            if (CheckValidity(input, input.width, input.height, newPoint, sourceColor, tollerance))
+                q.Enqueue(newPoint);
+
+            iterations++;
+        }
+    }
+
+    static bool CheckValidity(Texture2D texture, int width, int height, Point p, Color sourceColor, float tollerance)
+    {
+        if (p.x < 0 || p.x >= width)
+        {
+            return false;
+        }
+        if (p.y < 0 || p.y >= height)
+        {
+            return false;
+        }
+
+        var color = texture.GetPixel(p.x, p.y);
+
+        var distance = Mathf.Abs(color.r - sourceColor.r) + Mathf.Abs(color.g - sourceColor.g) + Mathf.Abs(color.b - sourceColor.b);
+        return distance <= tollerance;
+    }
+}
