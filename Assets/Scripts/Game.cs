4af3a54a6c31023c3b33287f2be567606086d0b9
Dozed12 <up201607929@fe.up.pt>
2019-12-04 21:29:23 +0000

    Calculate Design Difference

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -14,4 +14,25 @@ public static class Game
 
     //Enemy Nation
     public static Nation them;
+
+    //Calculate Design Difference
+    public static void DesignDifference(Design a, Design b)
+    {
+        int diff = 0;
+
+        //Calculate characteristics difference
+        for (int i = 0; i < a.characteristics.Count; i++)
+        {
+            diff += (b.characteristics[i].trueValue - a.characteristics[i].trueValue) * (int)a.characteristics[i].importance;
+        }
+
+        //Multiply by design importance
+        diff *= (int)a.importance;
+    }
+
+    //Calculate Nation Difference
+    public static void NationDifference()
+    {
+        
+    }
 }
