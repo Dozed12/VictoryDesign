f7e29e3ecdd7336dccbcadbe7816a1fc83d4e055
Dozed12 <up201607929@fe.up.pt>
2019-12-10 02:37:36 +0000

    Months Age Tooltip

--- Assets/Scripts/GameHolder.cs
+++ Assets/Scripts/GameHolder.cs
@@ -170,6 +170,7 @@ public class GameHolder : MonoBehaviour
         tooltips.Add("Importance", "Importance of design or characteristic,\nthe higher the importance the bigger the impact.");
         tooltips.Add("TrueIcon", "True value of a characteristic from -10 to 10. As the design is used more and more\n we will have better information about its characteristics performance");
         tooltips.Add("EstimateIcon", "Estimate value of a characteristic from -2 to 2. \nCorresponds to the expected true value of the characteristic.\n -2 \t-> \t-10 to -5\n -1 \t-> \t-10 to 0\n 0 \t-> \t-5 to 5\n 1 \t-> \t0 to 10\n 2 \t-> \t5 to 10");
+        tooltips.Add("Months", "Age of the design in months and the estimate age at which it will get replaced.");
     }
 
     //Setup new Game
