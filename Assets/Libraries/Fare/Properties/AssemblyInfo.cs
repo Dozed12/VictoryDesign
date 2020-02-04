a239e53178962b750edd17caacee215330effda4
Dozed12 <up201607929@fe.up.pt>
2020-02-04 18:41:11 +0000

    Remove CLS Compliancy

--- Assets/Libraries/Fare/Properties/AssemblyInfo.cs
+++ Assets/Libraries/Fare/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 //// COM, set the ComVisible attribute to true on that type.
 
 [assembly: ComVisible(false)]
-[assembly: CLSCompliant(true)]
+[assembly: CLSCompliant(false)]
 
 //// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("b9c2448c-21d1-4b22-bcba-c72b36adbf88")]
