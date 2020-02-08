7b658751263ca608336f4c6f2d46d8908d22b973
Dozed12 <up201607929@fe.up.pt>
2020-02-08 19:18:11 +0000

    Generate History Turns

--- Assets/Scripts/History.cs
+++ Assets/Scripts/History.cs
@@ -13,4 +13,24 @@ public static class History
     public static int ally1Turn;
     public static int ally2Turn;
     public static int ally3Turn;
+
+    //Turn of War
+    public static int warTurn;
+
+    public static void SetupHistory()
+    {
+        //Generate Proxy Turn
+        proxy1Turn = 6 + UnityEngine.Random.Range(-1,1 + 1);
+        proxy2Turn = 8 + UnityEngine.Random.Range(-1,1 + 1);
+        proxy3Turn = 16 + UnityEngine.Random.Range(-1,1 + 1);
+
+        //Generate Allies Turn
+        ally1Turn = 10 + UnityEngine.Random.Range(-1,1 + 1);
+        ally2Turn = 12 + UnityEngine.Random.Range(-1,1 + 1);
+        ally3Turn = 14 + UnityEngine.Random.Range(-1,1 + 1);
+
+        //Generate War Turn
+        warTurn = 20 + UnityEngine.Random.Range(-1,1 + 1);
+    }
+
 }
