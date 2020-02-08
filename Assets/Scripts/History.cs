ec94115138d5943901d4667a46cbc1581e316913
Dozed12 <up201607929@fe.up.pt>
2020-02-08 00:19:46 +0000

    Basic History Class

---
+++ Assets/Scripts/History.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class History
+{
+    //Turns of Proxy Wars
+    public static int proxy1Turn;
+    public static int proxy2Turn;
+    public static int proxy3Turn;
+
+    //Turn of Enemy Allies 
+    public static int ally1Turn;
+    public static int ally2Turn;
+    public static int ally3Turn;
+}
