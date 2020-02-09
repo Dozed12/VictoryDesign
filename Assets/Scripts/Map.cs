47b06a750260545b3b3d9714a38aa5de97abb433
Dozed12 <up201607929@fe.up.pt>
2020-02-09 15:32:43 +0000

    Setup unification/ally/revenge regions

--- Assets/Scripts/Map.cs
+++ Assets/Scripts/Map.cs
@@ -7,26 +7,18 @@ using UnityEngine.UI;
 public class Region
 {
     public Point point;
-    public bool occupied;
+    public bool unified = false;
+    public bool ally = false;
+    public bool occupied = false;   
 
-    public Region(Point point, bool occupied)
+    public Region(Point point)
     {
         this.point = point;
-        this.occupied = occupied;
     }
 }
 
 public static class Map
 {
-    //Pre war expansion stage (before player war, 1 for each nation)
-    public static int expansionStage;
-
-    //Pre war expansion stage positions
-    public static List<Region> prewarStagePositions = new List<Region>()
-    {
-        { new Region(new Point(850, 92), false)}
-    };
-
     //Stage of expansion from 0 to 6 (0 pre war, 6 only capital left)
     public static int warStage;
 
@@ -35,17 +27,35 @@ public static class Map
     {
         { new List<Region>()
             {
-                new Region( new Point(272, 333), false),
-                new Region( new Point(366, 333), false),
-                new Region( new Point(450, 387), false),
-                new Region( new Point(533, 431), false),
-                new Region( new Point(628, 400), false),
-                new Region( new Point(692, 360), false),
-                new Region( new Point(800, 351), false),
+                new Region( new Point(272, 333)),
+                new Region( new Point(366, 333)),
+                new Region( new Point(450, 387)),
+                new Region( new Point(533, 431)),
+                new Region( new Point(628, 400)),
+                new Region( new Point(692, 360)),
+                new Region( new Point(800, 351)),
             }
         }
         //TODO Rest of regions
     };
+
+    //Stage of unification positions
+    public static List<Region> unificationPositions = new List<Region>()
+    {
+        new Region( new Point(850, 92)),
+        new Region( new Point(449, 138))
+    };
+
+    //Stage of ally positions
+    public static List<Region> allyPositions = new List<Region>()
+    {
+        new Region( new Point(269, 30)),
+        new Region( new Point(398, 270)),
+        new Region( new Point(247, 192))
+    };
+
+    //Stage of revenge position
+    public static Region revengePosition = new Region( new Point(695, 200));
 
     //Build map at current stage (an optimized version could just increment the paint in case it's enemy expansion[since rest of map will stay the same])
     public static Texture2D BuildMap(Texture2D map)
@@ -115,5 +125,11 @@ public static class Map
         //If more progress repeat process
         if (num - 1 > 0)
             ProgressWar(num - 1);
+    }
+
+    //Progress Unification
+    public static void ProgressUnification(int num)
+    {
+        unificationPositions[num].occupied = true;
     }
 }
