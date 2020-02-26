64dcc95a9c9f00ab430318b6328bef0cc80c0e89
Dozed12 <up201607929@fe.up.pt>
2020-02-26 02:35:55 +0000

    Instructions for mirrorer.py

--- Assets/StreamingAssets/ModelParts/mirrorer.py
+++ Assets/StreamingAssets/ModelParts/mirrorer.py
@@ -3,15 +3,16 @@ import glob, os
 import sys
 
 def flip_image(image_path, saved_location):
-    """
-    Flip or mirror the image
-    @param image_path: The path to the image to edit
-    @param saved_location: Path to save the cropped image
-    """
     image_obj = Image.open(image_path)
     rotated_image = image_obj.transpose(Image.FLIP_LEFT_RIGHT)
     rotated_image.save(saved_location)
+
 if __name__ == '__main__':
+
+    if len(sys.argv) != 2:
+        print ("python mirrorer.py <folder>")
+        exit()
+
     os.chdir(os.getcwd() + "/" + sys.argv[1])
     for file in glob.glob("*.png"):
         image = file
